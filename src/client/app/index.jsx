d3f105d41bfab85ccecc11934b9a986a98de6680
szheng <sharon.zheng@live.com>
2016-09-25 00:35:52 -0400

    setup react

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,10 @@
-console.log('hello world');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <p>please work</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
