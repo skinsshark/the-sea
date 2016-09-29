afd25e93d1db9e9a1db6ee461900958fa623752b
szheng <sharon.zheng@live.com>
2016-09-28 21:11:39 -0400

    update Fish component

--- src/client/app/Fish.jsx
+++ src/client/app/Fish.jsx
@@ -8,8 +8,7 @@ class Fish extends React.Component {
     const basicProps = {
       alt: name,
     };
-    const imageURL = "/the-sea/src/client/images/" +
-      name + ".png";
+    const imageURL = "https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-9/12249813_1086012711439358_6608414893079548960_n.jpg?oh=49a8e005f262d3ef1f3c2b9640263405&oe=587AF613";
 
     return (
       <Col xs={12} md={3}>
